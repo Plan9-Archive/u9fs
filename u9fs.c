83ef40bce612f70660049628f2b5491b45bf81ea
Yaroslav Kolomiiets <yarikos@gmail.com>
2021-02-14 21:10:55 +0000

    create directories with at least user read mode set
    When replica/applylog creates directories it does create(name, DMDIR) followed by dirwstat() to set actual mode. Plan 9 file servers are fine with that; u9fs implements such creates as mkdir+opendir, and the later fails if the mode doesn't allow reads.

--- u9fs.c
+++ u9fs.c
@@ -1648,7 +1648,7 @@ usercreate(Fid *fid, char *elem, int omode, long perm, char **ep)
 			return -1;
 		}
 		/* race */
-		if(mkdir(npath, perm&0777) < 0){
+		if(mkdir(npath, perm&0777|0400) < 0){
 			*ep = strerror(errno);
 			free(npath);
 			return -1;
